3616bdb8574a357c398c7634e787225bb7c7f24e
optimized faisal <optimizedfaisal42@gmail.com>
2019-02-15 20:05:15 +0600

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.master')
+<!-- Page load hole layouts folder er master.blade.php file ta run hobe -->
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
